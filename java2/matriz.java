01b162d690cf3341ff4466503a00496add55609c
isaac1006 <iarprogramador@gmail.com>
2024-03-19 18:59:54 -0500

    impresion de matriz

--- java2/matriz.java
+++ java2/matriz.java
@@ -3,31 +3,34 @@ package java2;
 import java.util.Scanner;
 
 public class matriz {
-      public static void main(String[]args){
+    public static void main(String[] args) {
+        // Crear un objeto Scanner para leer la entrada del usuario
+        Scanner scanner = new Scanner(System.in);
+        // Crear una matriz 4x4
 
-        //crear un objeto scanner para leer la entrada del main
-        Scanner scanner=new Scanner(System.in);
+        int[][] matriz = new int[4][4];
 
-        //crear una Matriz 4*4
-        int [][]matriz=new int[4][4];
-
-        //solicitar al usurio q ingrese numeros para la matriz
-        System.out.println("ingresar los numeros en la matriz");
-        for (int i=0;i<4;i++){
-            for(int j=0;j<4;j++){
-                System.out.println("ingresar los numeros a la matriz");
-
-                matriz[i][j]=scanner.nextInt();
+        // Solicitar al usuario que ingrese números para la matriz //
+        System.out.println("Ingrese los números en la matriz:");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                // Solicitar al usuario que ingrese los números para cada posición de la matriz //
+                System.out.println("Ingrese el número para la posición (" + i + ", " + j + "):");
+                matriz[i][j] = scanner.nextInt();
             }
         }
 
-        //imprimir la matriz despues 
-        System.out.println("\n la mtriz ingresada es:");
-        for(int i=0;i<4;i++){
-            for(int j=0;j<4;j++){
-                System.out.println(matriz[i][j] + "\t" );
+        // Imprimir la matriz ingresada
+        System.out.println("\nLa matriz ingresada es:");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                // Imprimir cada elemento de la matriz separado por una tabulación //
+                System.out.print(matriz[i][j] + "\t");
             }
+            // Imprimir una nueva línea al final de cada fila //
+            System.out.println();
         }
-    }
     
+        scanner.close();
+    }
 }
