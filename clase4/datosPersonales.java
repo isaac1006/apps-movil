55fa9bdade65a83997519c8a18f7a22190a239c9
isaac1006 <iarprogramador@gmail.com>
2024-03-26 18:31:44 -0500

    creacion clase 4

---
+++ clase4/datosPersonales.java
@@ -0,0 +1,14 @@
+package clase4;
+import javax.swing.*;
+
+public class datosPersonales {
+
+    public static void main(String[] args) {
+        // solicitar al ususario ingresar el nombre //
+
+        String nombre=JOptionPane.showInputDialog("ingres el nombre ");
+        int edad = Integer.parseInt(JOptionPane.showInputDialog("ingrese la edad"));
+        
+    }
+    
+}
