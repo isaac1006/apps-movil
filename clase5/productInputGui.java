4c1215960c11ab73f528cb7a6840827caeae0204
isaac1006 <iarprogramador@gmail.com>
2024-04-02 19:32:24 -0500

    clase5 final

--- clase5/productInputGui.java
+++ clase5/productInputGui.java
@@ -1,64 +1,63 @@
 package clase5;
 import javax.swing.*;
-public class productInputGui {
 
-    /**
-     * @param args
-     */
+public class  productInputGui {
+
     public static void main(String[] args) {
-        // crear ventana  //
-        JFrame frame=new JFrame("INGRESAR PRODUCTO");
+        // crear ventana //
+        JFrame frame = new JFrame("INGRESAR PRODUCTO");
         frame.setSize(700, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Panel principal //
-        JPanel panel=new JPanel();
+        JPanel panel = new JPanel();
         frame.add(panel);
         colocarComponentes(panel);
 
         // mostrar ventana //
         frame.setVisible(true);
-        // metodo para agregar componentes a la ventana //
-        private static void colocarComponentes(JPanel panel){
-            panel.setLayout(null);
-            // etiqueta y campo de texto para el nombre del producto // 
-
-            JLabel nameLabel= new JLabel("NOMBRE DEL PRODUCTO");
-            nameLabel.setBounds(10,20,80,25);
-            panel.add(nameLabel);
-
-            // campo //
-            JTextField nameText=new JTextField(20);
-            nameText.setBounds(10, 20, 165, 25);
-            panel.add(nameText);
-            //etiqueta //
-
-            JLabel cantidadLabel = new JLabel("CANTIDAD PRODUCTO");
-            cantidadLabel.setBounds(10, 50, 80, 25);
-            panel.add(cantidadLabel);
-            
-            //campo //
-            JTextField cantidadText=new JTextField(20);
-            cantidadText.setBounds(100, 50, 165, 25);
-            panel.add(cantidadText);
-
-            // etiqueta //
-            JLabel precioLabel= new JLabel("PRECIO");
-            precioLabel.setBounds(10, 80, 80, 25);
-            panel.add(precioLabel);
-
-            // campo // 
-            JTextField precioText=new JTextField(20);
-            precioText.setBounds(100, 80, 165, 25);
-            panel.add(precioText);
-
-        }
-        
-        
     }
 
-    private static void PlaceComponents(JPanel panel) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'PlaceComponents'");
+    // método para agregar componentes a la ventana //
+    private static void colocarComponentes(JPanel panel) {
+        panel.setLayout(null);
+        // etiqueta y campo de texto para el nombre del producto // 
+
+        JLabel nameLabel= new JLabel("NOMBRE DEL PRODUCTO");
+        nameLabel.setBounds(10,20,150,25);
+        panel.add(nameLabel);
+
+        // campo //
+        JTextField nameText=new JTextField(20);
+        nameText.setBounds(170, 20, 165, 25);
+        panel.add(nameText);
+
+        // etiqueta //
+        JLabel cantidadLabel = new JLabel("CANTIDAD PRODUCTO");
+        cantidadLabel.setBounds(10, 50, 150, 25);
+        panel.add(cantidadLabel);
+        
+        // campo //
+        JTextField cantidadText=new JTextField(20);
+        cantidadText.setBounds(170, 50, 165, 25);
+        panel.add(cantidadText);
+
+        // etiqueta //
+        JLabel precioLabel= new JLabel("PRECIO");
+        precioLabel.setBounds(10, 80, 150, 25);
+        panel.add(precioLabel);
+
+        // campo // 
+        JTextField precioText=new JTextField(20);
+        precioText.setBounds(170, 80, 165, 25);
+        panel.add(precioText);
+
+        // boton de insertar // 
+        JButton inserButton= new JButton(" insertar Datos");
+        inserButton.setBounds(100,120, 80, 25);
+        panel.add(inserButton);
+        
+
     }
+
 }
